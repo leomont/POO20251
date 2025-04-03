5f67c3287388d534ecf3a883141c34aac534bcf0
SantiagoN <orangesant24@gmail.com>
2025-04-02 19:51:35 -0500

    Santiago N dias del mes

---
+++ POO20251/src/main/java/DiasDelMes.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DiasDelMes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese el número del mes (1-12): ");
+        int mes = scanner.nextInt();
+        int dias;
+        switch (mes) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                dias = 31;
+                break;
+            case 4: case 6: case 9: case 11:
+                dias = 30;
+                break;
+            case 2:
+                dias = 28; // No se considera años bisiestos en este caso
+                break;
+            default:
+                System.out.println("Mes no válido. Ingrese un número entre 1 y 12.");
+                scanner.close();
+                return;
+        }
+        
+        System.out.println("El mes " + mes + " tiene " + dias + " días.");
+       
+        scanner.close();
+    }
+}
