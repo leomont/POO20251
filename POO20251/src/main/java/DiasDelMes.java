c938a18df1dcd100d27deeba5329d9b599a27ef5
leomont <leonardo.montesm@gmail.com>
2025-04-02 20:08:27 -0500

    agregar modificacion al dia del mes

--- POO20251/src/main/java/DiasDelMes.java
+++ POO20251/src/main/java/DiasDelMes.java
@@ -10,12 +10,13 @@ public class DiasDelMes {
         switch (mes) {
             case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                 dias = 31;
-                break;
+
             case 4: case 6: case 9: case 11:
                 dias = 30;
-                break;
+
             case 2:
                 dias = 28; // No se considera años bisiestos en este caso
+                System.out.println("Mes de 28 dias");
                 break;
             default:
                 System.out.println("Mes no válido. Ingrese un número entre 1 y 12.");
@@ -23,8 +24,7 @@ public class DiasDelMes {
                 return;
         }
         
-        System.out.println("El mes " + mes + " tiene " + dias + " días.");
-       
-        scanner.close();
+
+
     }
 }
