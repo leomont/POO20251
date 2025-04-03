080e4571b56ac3893c98b827004a7c59ca926ca5
Alejandro villa <alejandro.villa42991@ucaldas.edu.co>
2025-04-02 19:27:36 -0500

    Cargue el archivo del ejercicio de notas

---
+++ POO20251/src/main/java/calculoNotas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class calculoNotas{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese las notas");
+        double nota1 = sc.nextDouble();
+        double nota2 = sc.nextDouble();
+
+        if(nota1 + nota2 ==7){
+            System.out.println("Aprueba");
+        }
+        else{
+            System.out.println("Reprueba");
+        }
+    }
+}
